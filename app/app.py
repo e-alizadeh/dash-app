142b6240196e79e260947deabded71b9c64bf57e
ealizadeh <alizadeh.essi@gmail.com>
2021-08-22 18:00:22 -0400

    Add loading component surronding the scatter plot. Apply pre-commit.

--- app/app.py
+++ app/app.py
@@ -1,13 +1,12 @@
 from enum import Enum
 
-import plotly.express as px
-from umap import UMAP
-from sklearn.manifold import TSNE
-
 import dash
-from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+from dash.dependencies import Input, Output
+from sklearn.manifold import TSNE
+from umap import UMAP
 
 SEED = 0
 
@@ -18,49 +17,57 @@ class DimReductionMethods(Enum):
 
 
 def app_layout() -> html.Div:
-    return html.Div([
-        dcc.RadioItems(
-            id="selected-method-radio-item",
-            options=[
-                {'label': DimReductionMethods.UMAP.value, 'value': DimReductionMethods.UMAP.value},
-                {'label': DimReductionMethods.TSNE.value, 'value': DimReductionMethods.TSNE.value}
-            ],
-            value='tsne'
-        ),
-        dcc.Graph(
-            id="scatter-plot"
-        )
-    ])
-
-
-app = dash.Dash()
-
-
-@app.callback(
-    Output("scatter-plot", "figure"),
-    [Input("selected-method-radio-item", "value")]
-)
-def add_graph(selected_method):
-    df = px.data.iris()
-    features = df.loc[:, :'petal_width']
-
-    if selected_method == DimReductionMethods.UMAP.value:
-        umap = UMAP(n_components=2, init='random', random_state=SEED)
-        projections = umap.fit_transform(features)
-    elif selected_method == DimReductionMethods.TSNE.value:
-        tsne = TSNE(n_components=2, random_state=SEED)
-        projections = tsne.fit_transform(features)
-    else:
-        umap = UMAP(n_components=2, init='random', random_state=SEED)
-        projections = umap.fit_transform(features)
-
-    fig = px.scatter(
-        projections, x=0, y=1,
-        color=df.species, labels={'color': 'species'}
+    return html.Div(
+        [
+            dcc.RadioItems(
+                id="selected-method-radio-item",
+                options=[
+                    {
+                        "label": DimReductionMethods.UMAP.value,
+                        "value": DimReductionMethods.UMAP.value,
+                    },
+                    {
+                        "label": DimReductionMethods.TSNE.value,
+                        "value": DimReductionMethods.TSNE.value,
+                    },
+                ],
+                value="tsne",
+            ),
+            dcc.Loading(
+                id="loading-1",
+                type="default",
+                children=html.Div(dcc.Graph(id="scatter-plot")),
+            ),
+        ]
     )
-    return fig
+
+
+def app_callbacks(app):
+    @app.callback(
+        Output("scatter-plot", "figure"), [Input("selected-method-radio-item", "value")]
+    )
+    def add_graph(selected_method):
+        df = px.data.iris()
+        features = df.loc[:, :"petal_width"]
+
+        if selected_method == DimReductionMethods.UMAP.value:
+            umap = UMAP(n_components=2, init="random", random_state=SEED)
+            projections = umap.fit_transform(features)
+        elif selected_method == DimReductionMethods.TSNE.value:
+            tsne = TSNE(n_components=2, random_state=SEED)
+            projections = tsne.fit_transform(features)
+        else:
+            umap = UMAP(n_components=2, init="random", random_state=SEED)
+            projections = umap.fit_transform(features)
+
+        fig = px.scatter(
+            projections, x=0, y=1, color=df.species, labels={"color": "species"}
+        )
+        return fig
 
 
 if __name__ == "__main__":
+    app = dash.Dash()
     app.layout = app_layout()
+    app_callbacks(app)
     app.run_server(debug=True, use_reloader=False)
