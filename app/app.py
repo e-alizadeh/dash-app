a1d41aa0c0e0ae6b37bbaabe492224dd7805a3da
ealizadeh <alizadeh.essi@gmail.com>
2021-08-25 20:32:47 -0400

    Comment out FastAPI implementation.

--- app/app.py
+++ app/app.py
@@ -5,12 +5,13 @@ from fastapi import FastAPI
 from starlette.middleware.wsgi import WSGIMiddleware
 
 if __name__ == "__main__":
-    app = dash.Dash(__name__, requests_pathname_prefix="/dash/")
+    # app = dash.Dash(__name__, requests_pathname_prefix="/dash/")
 
+    app = dash.Dash()
     app.layout = app_layout()
     generate_callbacks(app)
-    # app.run_server(debug=True, use_reloader=False)
+    app.run_server(debug=True, use_reloader=False)
 
-    server = FastAPI()
-    server.mount("/dash", WSGIMiddleware(app.server))
-    uvicorn.run(server)
+    # server = FastAPI()
+    # server.mount("/dash", WSGIMiddleware(app.server))
+    # uvicorn.run(server)
