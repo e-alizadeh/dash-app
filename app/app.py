a3026f9d8dbf01b9300a5d1ec2219717491fceb3
ealizadeh <alizadeh.essi@gmail.com>
2021-09-05 23:31:10 -0400

    Add external stylesheet.

--- app/app.py
+++ app/app.py
@@ -1,13 +1,18 @@
 import dash
-import uvicorn as uvicorn
+
+# import uvicorn as uvicorn
 from dashboard import app_layout, generate_callbacks
-from fastapi import FastAPI
-from starlette.middleware.wsgi import WSGIMiddleware
+
+# from fastapi import FastAPI
+# from starlette.middleware.wsgi import WSGIMiddleware
+
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
 
 if __name__ == "__main__":
     # app = dash.Dash(__name__, requests_pathname_prefix="/dash/")
 
-    app = dash.Dash("ABC")
+    app = dash.Dash("ABC", external_stylesheets=external_stylesheets)
     server = app.server
     app.layout = app_layout(app)
     generate_callbacks(app)
