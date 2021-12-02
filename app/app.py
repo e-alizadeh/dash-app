73cb60c34f9eac4d4e582b5262fb514ee921cdb8
ealizadeh <alizadeh.essi@gmail.com>
2021-12-02 15:04:53 -0500

    Update app dash server handling and use custom host and port number. Remove commented codes.

--- app/app.py
+++ app/app.py
@@ -1,22 +1,12 @@
 import dash
 import dash_bootstrap_components as dbc
-
-# import uvicorn as uvicorn
 from dashboard import app_layout, generate_callbacks
 
-# from fastapi import FastAPI
-# from starlette.middleware.wsgi import WSGIMiddleware
+app = dash.Dash("ABC", external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 
 if __name__ == "__main__":
-    # app = dash.Dash(__name__, requests_pathname_prefix="/dash/")
-
-    app = dash.Dash("ABC", external_stylesheets=[dbc.themes.BOOTSTRAP])
-    server = app.server
     app.layout = app_layout(app)
     generate_callbacks(app)
-    app.run_server(debug=True, use_reloader=False)
-
-    # server = FastAPI()
-    # server.mount("/dash", WSGIMiddleware(app.server))
-    # uvicorn.run(server)
+    app.run_server(debug=True, host="0.0.0.0", port=8080, use_reloader=False)
