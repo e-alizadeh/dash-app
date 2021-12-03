35e686b321809f30fb4fb9c57198c15350020256
ealizadeh <alizadeh.essi@gmail.com>
2021-12-02 21:54:20 -0500

    Select Zephyr theme from dash_bootstrap_components. Run Dash app server on port 8000.

--- app/app.py
+++ app/app.py
@@ -2,11 +2,11 @@ import dash
 import dash_bootstrap_components as dbc
 from dashboard import app_layout, generate_callbacks
 
-app = dash.Dash("ABC", external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash("ABC", external_stylesheets=[dbc.themes.ZEPHYR])
 server = app.server
 
 
 if __name__ == "__main__":
     app.layout = app_layout(app)
     generate_callbacks(app)
-    app.run_server(debug=True, host="0.0.0.0", port=8080, use_reloader=False)
+    app.run_server(debug=True, host="0.0.0.0", port=8000, use_reloader=False)
