ee103ecba2233900277d0bf7a8ba9a69bf737fd4
ealizadeh <alizadeh.essi@gmail.com>
2021-12-05 13:40:18 -0500

    Set debug flag to False.

--- app/app.py
+++ app/app.py
@@ -43,4 +43,4 @@ server = app.server
 if __name__ == "__main__":
     app.layout = app_layout(app)
     generate_callbacks(app)
-    app.run_server(debug=True, host="0.0.0.0", port=8000, use_reloader=False)
+    app.run_server(debug=False, host="0.0.0.0", port=8000, use_reloader=False)
