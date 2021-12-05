9abc0eb6e5e6ba69d0e31d2d6c1ea6341cef107e
ealizadeh <alizadeh.essi@gmail.com>
2021-12-05 00:01:22 -0500

    Add GA tracking to dash app

--- app/app.py
+++ app/app.py
@@ -2,10 +2,16 @@ import dash
 import dash_bootstrap_components as dbc
 from dashboard import app_layout, generate_callbacks
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.ZEPHYR])
+
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[dbc.themes.ZEPHYR],
+    external_scripts=[
+        "https://gist.githubusercontent.com/e-alizadeh/4b86f02ae6f6ea2a0c39a7e2ac1659eb/raw/023b713f2bf32a78a0282e4de8875e5efc2c9d58/gtag.js"
+    ]
+)
 app.title = "Simple Dash App"
 server = app.server
-
 
 if __name__ == "__main__":
     app.layout = app_layout(app)
